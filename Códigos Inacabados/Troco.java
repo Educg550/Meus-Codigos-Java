f806f8fc7da343a3c3ac7c1ca792c9083a7e1693
Educg550 <eduardo9cruz@gmail.com>
2020-06-25 08:56:38 -0300

    Upload de um exercício retirado do URI Online Judge (Problema 1180 - Java)

--- Códigos Inacabados/Troco.java
+++ Códigos Inacabados/Troco.java
@@ -1,6 +1,6 @@
-/*/ESTE PROGRAMA CALCULA O TROCO QUE DEVERÁ SER FORNECIDO AO
+/*ESTE PROGRAMA CALCULA O TROCO QUE DEVERA SER FORNECIDO AO
 CLIENTE QUE PAGA UM PRODUTO COM CERTA QUANTIA EM DINHEIRO, 
-INFORMANDO AO USUÁRIO O NÚMERO DE CÉDULAS E MOEDAS DEVOLVIDAS/*/
+INFORMANDO AO USUARIO O NUMERO DE CEDULAS E MOEDAS DEVOLVIDAS*/
 
 import java.util.Scanner;
 public class Troco
@@ -17,13 +17,80 @@ public class Troco
 
 		if (pago < valor)
 		{
-			System.out.println ("O cliente nao pagou a quantia necessaria!")
+			System.out.println ("O cliente nao pagou a quantia necessaria!");
 		}
 		else
 		{
-			while (pago >= 100)
+			//CONTAGEM DO TROCO E ARMAZENAMENTO DO NUMERO DE CEDULAS E MOEDAS NO ARRAY "troco" (cedulas na linha 0 e moedas na linha 1)
+			int[][] troco = new int[2][6];
+
+			while (pago - 100 >= valor)
 			{
-				//RETOMAR DAQUI//
+				troco [0][0]++;
+				pago -= 100;
+			}
+			while (pago - 50 >= valor)
+			{
+				troco [0][1]++;
+				pago -= 50;
+			}
+			while (pago - 20 >= valor)
+			{
+				troco [0][2]++;
+				pago -= 20;
+			}
+			while (pago - 10 >= valor)
+			{
+				troco [0][3]++;
+				pago -= 10;
+			}
+			while (pago - 5 >= valor)
+			{
+				troco [0][4]++;
+				pago -= 5;
+			}
+			while (pago - 2 >= valor)
+			{
+				troco [0][5]++;
+				pago -= 2;
+			}
+			while (pago - 1 >= valor)
+			{
+				troco [1][0]++;
+				pago -= 1;
+			}
+			while (pago - 0.5 >= valor)
+			{
+				troco [1][1]++;
+				pago -= 0.5;
+			}
+			while (pago - 0.25 >= valor)
+			{
+				troco [1][2]++;
+				pago -= 0.25;
+			}
+			while (pago - 0.1 >= valor)
+			{
+				troco [1][3]++;
+				pago -= 0.1;
+			}
+			while (pago - 0.05 >= valor)
+			{
+				troco [1][4]++;
+				pago -= 0.05;
+			}
+			while (pago - 0.01 >= valor)
+			{
+				troco [1][5]++;
+				pago -= 0.01;
+			}
+
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 6; j++)
+				{
+					//RETOMAR DAQUI
+				}
 			}
 		}
 	}
---
+++ Códigos Terminados/Menor_e_Posicao.java
@@ -0,0 +1,37 @@
+/*
+Faça um programa que leia um valor N. Este N será o tamanho de um vetor X[N]. 
+A seguir, leia cada um dos valores de X, encontre o menor elemento deste vetor 
+e a sua posição dentro do vetor, mostrando esta informação.
+*/
+
+import java.util.Scanner;
+public class Menor_e_Posicao
+{
+    public static void main (String [] args)
+	{
+		Scanner leitor = new Scanner (System.in);
+
+        System.out.println ("Insira o valor N: ");
+        int [] n = new int [leitor.nextInt()];
+
+        int menor = 0, posicao = 0;
+
+        for (int i = 0; i < n.length; i++)
+        {
+            n[i] = leitor.nextInt();
+
+            if (i == 0)
+            {
+                menor = n[i];
+                posicao = i;
+            }
+            else if (n[i] < menor)
+            {
+                menor = n[i];
+                posicao = i;
+            }
+        }
+
+        System.out.printf ("Menor valor: %d\nPosicao: %d", menor, posicao);
+	}
+}
